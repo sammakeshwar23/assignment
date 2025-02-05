00f1fc1906820a72b1954cddd0057526073b95cf
sammakeshwar23 <shubhammakeshwar23@gmail.com>
2025-02-05 14:10:47 +0530

    quesion1 and question3 added

---
+++ question3.js
@@ -0,0 +1,33 @@
+db.sales.aggregate([
+    { 
+      $unwind: "$items" 
+    },
+    { 
+      $project: {
+        store: 1,
+        month: { $dateToString: { format: "%Y-%m", date: "$date" } },
+        revenue: { $multiply: ["$items.quantity", "$items.price"] },
+        price: "$items.price"
+      }
+    },
+    { 
+      $group: {
+        _id: { store: "$store", month: "$month" },
+        totalRevenue: { $sum: "$revenue" },
+        averagePrice: { $avg: "$price" }
+      }
+    },  
+    { 
+      $project: {
+        _id: 0,
+        store: "$_id.store",
+        month: "$_id.month",
+        totalRevenue: 1,
+        averagePrice: 1
+      }
+    },
+    { 
+      $sort: { store: 1, month: 1 }
+    }
+  ])
+  
